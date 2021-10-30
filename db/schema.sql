31dd3769c2369a6deb3fe660145aa68ab96fdd5f
Jessica Galarza <jessicaga941@gmail.com>
2021-10-30 17:04:32 -0400

    modified schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE company_db;
+DROP DATABASE IF EXISTS eTracker_db;
+CREATE DATABASE eTracker_db;
 
-USE company_db;
+USE eTracker_db;
 
 CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- index.js
+++ index.js
@@ -20,18 +20,6 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the eTracker_db database.`)
 );
-
-db.query(`DELETE FROM course_names WHERE id = ?`, 3, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(result);
-  });
-  
-  // Query database
-  db.query('SELECT * FROM course_names', function (err, results) {
-    console.log(results);
-  });
   
   // Default response for any other request (Not Found)
   app.use((req, res) => {
@@ -43,4 +31,3 @@ db.query(`DELETE FROM course_names WHERE id = ?`, 3, (err, result) => {
   });
   
 
-module.exports = db;
